1209c8c8fc09442618ecbfa493a144c2aacd1fe0
h.seifi <seyedh.seifi@gmail.com>
2025-08-11 09:48:34 +0330

    Edit accardeon

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -16,11 +16,19 @@ export const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
         onClick={() => setIsOpen(!isOpen)}
       >
         <span className="text-lg font-bold text-[#00FFA6] flex items-center">
-          <PlusIcon
-            className={`w-5 h-5 mr-2 transition-transform duration-300 ${
-              isOpen ? "rotate-45" : ""
-            }`}
-          />
+          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_226_2273"  maskUnits="userSpaceOnUse" x="0" y="0" width="14" height="14">
+              <rect width="14" height="14" fill="#D9D9D9" />
+            </mask>
+            <g mask="url(#mask0_226_2273)">
+              <path d="M3.5 7.58329V6.41663H10.5V7.58329H3.5Z" fill="#00FFA6" />
+            </g>
+          </svg>
+
+          {/* <PlusIcon
+            className={`w-5 h-5 mr-2 transition-transform duration-300 ${isOpen ? "rotate-45" : ""
+              }`}
+          /> */}
           {title}
         </span>
       </div>
